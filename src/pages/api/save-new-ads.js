4c8a04737a1d3d74d39e00b3f94fb6e5028250a1
Nihal Chaurasia <nihal@delimp.com>
2023-05-03 16:27:32 +0530

    Send Mail Functionality to a specific user

--- src/pages/api/save-new-ads.js
+++ src/pages/api/save-new-ads.js
@@ -1,10 +1,49 @@
+import sendgrid from "@sendgrid/mail";
 import connectDB from '../../config/db';
 import { generateUniqueId } from '../../helper/helpers';
 import Ad from '../../models/ad';
 
-// Use the Ad model here
+sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
+
+async function sendEmail(url, email) {
+    try {
+        await sendgrid.send({
+            to: "nhlchaurasia@gmail.com", //postmagnetmedia@gmail.com
+            from: "nihal@delimp.com",
+            subject: "Here's some new ads has been created",
+            html: `<!DOCTYPE html>
+            <html>
+              <head>
+                <title>New Ad Preview Created</title>
+              </head>
+              <body>
+                <p>Dear postmagnetmedia,</p>
+                <p>
+                  I am pleased to inform you that the ad preview has been created by <b>${email}</b>. I am excited to share
+                  with you the URL for the final ad preview:
+                  <a href="https://g-ad-next-js.vercel.app/${url}">https://g-ad-next-js.vercel.app/${url}</a>.
+                </p>
+                <p>
+                  I would appreciate it if you could take a moment to review the ad.
+                  Thank you for your continued support.
+                </p>
+                <p>Best regards,</p>
+                <p>Nobody</p>
+              </body>
+            </html>
+            `,
+            text: "body",
+            cc: 'nobody@delimp.com'
+        });
+    } catch (error) {
+        console.log(error.message)
+        // return res.status(error.statusCode || 500).json({ error: error.message });
+    }
+
+}
 
 connectDB();
+
 export default async function handler(req, res) {
     // Handle the API request here
     try {
@@ -37,9 +76,10 @@ export default async function handler(req, res) {
         });
         console.log(req.body.final_url)
         await ad.save();
+        await sendEmail(`resources/adspreview?id=${adId}`, req.body.email);
         res.status(201).json({ id: adId });
     } catch (err) {
         console.error(err);
-        res.status(500).json({ message: 'Internal server error' });
+        res.status(500).json({ message: err.message });
     }
 }
